7ba43e3e54e7e902757139a1f9a47ce7a0ce2590
SameerSrz <sameerriaz910@gmail.com>
2024-04-04 05:00:17 +0500

    aa

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ app.get("/", (req, res) => {
 
 
 // Start Server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () =>
   console.log(`Server Running on Port: http://localhost:${PORT}/`)
 );
+
+module.exports = app;
